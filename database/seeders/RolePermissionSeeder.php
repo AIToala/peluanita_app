f45f4ab2140f1e4ddec9f9874d01824941dc1c20
Andres Toala <ai_toala@outlook.com>
2025-02-27 17:09:29 -0500

    feat: Creating permissions and roles for 3 types of users: Clients, Admin and Employees

--- database/seeders/RolePermissionSeeder.php
+++ database/seeders/RolePermissionSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 
 class RolePermissionSeeder extends Seeder
@@ -13,6 +15,43 @@ class RolePermissionSeeder extends Seeder
      */
     public function run(): void
     {
-        
+        $permissions = [
+            'manage_users',
+            'manage_roles',
+            'manage_permissions',
+            'manage_clients',
+            'manage_employees',
+            'request_appointments',
+            'create_appointments',
+            'cancel_appointments',
+            'manage_appointments',
+            'manage_services',
+            'manage_attentions',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::create(['name' => $permission]);
+        }
+
+
+        $admin = Role::create(['name' => 'admin']);
+        $client = Role::create(['name' => 'cliente']);
+        $employee = Role::create(['name' => 'empleado']);
+
+
+        $admin->givePermissionTo(Permission::all());
+
+        $client->givePermissionTo([
+            'request_appointments',
+            'cancel_appointments',
+        ]);
+
+        $employee->givePermissionTo([
+            'create_appointments',
+            'cancel_appointments',
+            'manage_appointments',
+            'manage_attentions',
+            'manage_clients',
+        ]);
     }
 }
